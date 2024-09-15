5b19a4bb7946f545d1ad3a0868c954c8a5c416a9
João Victor <145297637+joao-vrds@users.noreply.github.com>
2024-09-15 10:35:13 -0300

    Add files via upload

---
+++ conversao.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const ebulicaoK float64 = 373.2
+
+func main() {
+
+	C := ebulicaoK - 273
+
+	fmt.Println("O ponto de ebulição da água de Kelvin para Celsius é: ", C)
+
+}
